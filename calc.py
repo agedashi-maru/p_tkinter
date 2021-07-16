a692962baf177f6dd960f6b7d98bcbf35f55b9d2
agedashi-maru <photocurrency.0402@gmail.com>
2021-07-16 09:54:35 +0900

    change : use eval func

--- calc.py
+++ calc.py
@@ -2,96 +2,57 @@ from tkinter import *
 from tkinter import ttk
 
 
-# 画面に表示する値
-label_value = ""
-# 計算用のリスト
-calc_list = []
+calc_str = ""
 
 def push_number_bnt(event):
     """
     数字ボタン用の処理
     """
-    global label_value
+    global calc_str
     
-    # 追記して再表示
-    label_value += str(event.widget["text"])
-    label_str.set(label_value)
+    calc_str += event.widget["text"]
+    label_str.set(event.widget["text"])
+    
 
 
 def push_operator_btn(event):
     """
     「+ - x ÷」ボタン用の処理
     """
-    global label_value
-    global calc_list
+    global calc_str
+    
+    text = event.widget["text"]
 
-    if len(calc_list) == 0:
-        if label_value == "":
-            return
-        
-        calc_list.append(int(label_value))
-        calc_list.append(event.widget["text"])
-        label_value = ""
-        label_str.set(event.widget["text"])
+    if text == "+":
+        calc_str += "+"
+    elif text == "-":
+        calc_str += "-"
+    elif text == "x":
+        calc_str += "*"
+    elif text == "÷":
+        calc_str += "/"
 
-    elif len(calc_list) == 1:
-        calc_list.append(event.widget["text"])
-        label_str.set(event.widget["text"])
-        label_value = ""
-
-    elif len(calc_list) == 2:
-        if calc_list[1] == "+":
-            label_value = calc_list[0] + int(label_value)
-        elif calc_list[1] == "-":
-            label_value = calc_list[0] - int(label_value)
-        elif calc_list[1] == "x":
-            label_value = calc_list[0] * int(label_value)
-        elif calc_list[1] == "÷":
-            label_value = calc_list[0] / int(label_value)
-
-        label_str.set(label_value)
-        calc_list = []
-        calc_list.append(label_value)
-        calc_list.append(event.widget["text"])
-        label_value=""
+    label_str.set(event.widget["text"])
 
 
 def push_equal_bnt(event):
     """
     「=」ボタン用の処理
     """
-    global label_value
-    global calc_list
+    global calc_str
 
-    if len(calc_list) <= 1:
-        label_str.set(label_value)
-    else:
-        if calc_list[1] == "+":
-            label_value = calc_list[0] + int(label_value)
-        elif calc_list[1] == "-":
-            label_value = calc_list[0] - int(label_value)
-        elif calc_list[1] == "x":
-            label_value = calc_list[0] * int(label_value)
-        elif calc_list[1] == "÷":
-            label_value = calc_list[0] / int(label_value)
-
-        label_str.set(label_value)
-        calc_list = []
-        calc_list.append(label_value)
-        # label_value = str(label_value)
-        label_value = ""
+    equal_str = eval(calc_str)
+    label_str.set(equal_str)
 
 
 def push_clear_bnt(event):
     """
     「c」ボタン用の処理
     """
-    global label_value
-    global calc_list
-
-    label_value = ""
-    calc_list = []
-    label_str.set(label_value)
+    global calc_str
+    
+    calc_str = ""
+    label_str.set("0")
 
 
 if __name__ == "__main__":
@@ -102,6 +63,7 @@ if __name__ == "__main__":
     frame.grid(column=0, row=0)
 
     label_str = StringVar()
+    label_str.set("0")
     
     # 入力内容の表示領域を定義
     lb = ttk.Label(frame, textvariable=label_str)
@@ -112,7 +74,7 @@ if __name__ == "__main__":
     text_num = 0
     for i in [4, 3, 2]:
         for j in [1, 2, 3]:
-            btn = ttk.Button(frame, text=j+text_num)
+            btn = ttk.Button(frame, text=str(j+text_num))
             btn.bind("<Button-1>", push_number_bnt)
             btn.grid(column=j, row=i)
         
